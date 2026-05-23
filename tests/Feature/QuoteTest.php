6d71421940ae1fbe7f9ea0c1f3a65ad67a73b28b
Ablebil <hafizhfaiq07@gmail.com>
2026-05-23 11:12:00 +0700

    test(quotes): add feature test for quotes flow

---
+++ tests/Feature/QuoteTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Quote;
+
+class QuoteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private User $admin;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutVite();
+        $this->admin = User::create([
+            "name" => "Admin",
+            "email" => "admin@example.com",
+            "password" => "password123",
+        ]);
+    }
+
+    public function test_anyone_can_view_quotes_list(): void
+    {
+        Quote::create([
+            "quote_text" => "Lorem ipsum dolor sit amet.",
+            "author" => "John Doe",
+        ]);
+
+        $response = $this->get("/quotes");
+
+        $response->assertStatus(200);
+        $response->assertSee("Lorem ipsum dolor sit amet.");
+    }
+
+    public function test_admin_can_create_new_quote(): void
+    {
+        $response = $this->actingAs($this->admin)->post("/quotes", [
+            "quote_text" => "To be or not to be.",
+            "author" => "William S.",
+        ]);
+
+        $response->assertRedirect("/quotes");
+
+        $this->assertDatabaseHas("quotes", [
+            "quote_text" => "To be or not to be.",
+            "author" => "William S.",
+        ]);
+    }
+
+    public function test_admin_can_update_quote(): void
+    {
+        $quote = Quote::create([
+            "quote_text" => "Old quote text.",
+            "author" => "Old Author",
+        ]);
+
+        $response = $this->actingAs($this->admin)->put("/quotes/{$quote->id}", [
+            "quote_text" => "Updated quote text.",
+            "author" => "New Author",
+        ]);
+
+        $response->assertRedirect("/quotes");
+
+        $this->assertDatabaseHas("quotes", [
+            "id" => $quote->id,
+            "quote_text" => "Updated quote text.",
+        ]);
+    }
+
+    public function test_admin_can_delete_quote(): void
+    {
+        $quote = Quote::create([
+            "quote_text" => "This quote will be deleted.",
+            "author" => "Jane Doe",
+        ]);
+
+        $response = $this->actingAs($this->admin)->delete(
+            "/quotes/{$quote->id}",
+        );
+
+        $response->assertRedirect("/quotes");
+
+        $this->assertDatabaseMissing("quotes", [
+            "id" => $quote->id,
+        ]);
+    }
+
+    public function test_guest_cannot_create_quote(): void
+    {
+        $response = $this->post("/quotes", [
+            "quote_text" => "Hack attempt.",
+            "author" => "Hacker",
+        ]);
+
+        $response->assertRedirect("/login");
+
+        $this->assertDatabaseMissing("quotes", [
+            "quote_text" => "Hack attempt.",
+        ]);
+    }
+}
